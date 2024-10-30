96e2e753c1ace605f7c64fa92e8858642b4b712e
dmorita00 <ingressmrt@gmail.com>
2024-10-30 11:49:50 +0900

    Fix typo in instructions on page.tsx

--- mrt-pj1/src/app/page.tsx
+++ mrt-pj1/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            ABCDEGet started by editing <code>src/app/page.tsx</code>.
+            1ABCDEGet started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>

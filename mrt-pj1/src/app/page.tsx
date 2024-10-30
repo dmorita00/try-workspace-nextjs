31ea7e9b230d02a8cb9df7cf5a43d7e6356058f1
dmorita00 <ingressmrt@gmail.com>
2024-10-30 10:02:41 +0900

    Fix typo in starting instructions

--- mrt-pj1/src/app/page.tsx
+++ mrt-pj1/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            ABCDGet started by editing <code>src/app/page.tsx</code>.
+            ABCDEGet started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
